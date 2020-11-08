651ec6fd2d2cbb0fd056f6c2d8536ecf515e3cec
DyonisiiBrytik <70817871+Dyonisii@users.noreply.github.com>
2020-11-08 22:05:04 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" id="root">
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+function   tick() {
+   
+   const element = (
+     
+     <div class="clock">
+       <h1 class="time">{ new Date().toLocaleTimeString() }</h1>
+       <h1 class="date">{ new Date().toLocaleDateString() }</h1>
+     </div>
+     
+   );
+   
+    
+   ReactDOM.render(
+     element,
+     document.getElementById('root')
+   );
+   
+ }
+ setInterval(tick, 1000);
